59d21a81c2bfcc2348926dad2d2844aa56a8cf9a
lawalafeez820 <lawalafeez820@gmail.com>
2022-10-17 22:57:29 -0700

    forth helper file

--- 4th_helper_file.c
+++ 4th_helper_file.c
@@ -1,103 +1,188 @@
-#include <stdlib.h>
-#include <stdarg.h>
-#include "holberton.h"
-/**
-  * found_char - converts a char to a string
-  * @c: char to convert
-  * Return: 2 byte string
-  */
-char *found_char(va_list c)
-{
-	char string[2];
-	char *p;
+#include "main.h"
 
-	p = string;
-	string[0] = va_arg(c, int);
-	string[1] = '\0';
-	return (p);
-}
+/****************** PRINT POINTER ******************/
 /**
-  * found_string - passes a string through
-  * @s: string to pass through
-  * Return: unmodified string
-  */
-char *found_string(va_list *s)
+ * print_pointer - Print the value of a pointer variable
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	char *string;
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
 
-	string = va_arg(s, char *);
-	if (string == NULL)
-		return ("(null)");
-	return (string);
-}
-/**
-  * found_percent - passes a % sign through
-  * Return: percent sign as a character string
-  */
-char *found_percent(void)
-{
-	char *string;
+	UNUSED(width);
+	UNUSED(size);
 
-	string = "%";
-	return (string);
-}
-/**
-  * found_int - converts integers into character arrays
-  * @npoint: integer to convert from va_args
-  * Return: pointer to string
-  */
-char *found_int(va_list npoint)
-{
-	int n;
-	int i, res, temp, expo, count;
-	char *string;
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
 
-	n = va_arg(npoint, int);
-	count = i = 0;
-	expo = 1;
-	n >= 0 ? (res = n * -1) : (res = n, count++);
-	temp = res;
-	while (temp <= -10)
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
 	{
-		expo *= 10;
-		temp /= 10;
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', length++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', length++;
+
+	ind++;
+
+	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
+	return (write_pointer(buffer, ind, length,
+		width, flags, padd, extra_c, padd_start));
+}
+
+/************************* PRINT NON PRINTABLE *************************/
+/**
+ * print_non_printable - Prints ascii codes in hexa of non printable chars
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[i] != '\0')
+	{
+		if (is_printable(str[i]))
+			buffer[i + offset] = str[i];
+		else
+			offset += append_hexa_code(str[i], buffer, i + offset);
+
+		i++;
+	}
+
+	buffer[i + offset] = '\0';
+
+	return (write(1, buffer, i + offset));
+}
+
+/************************* PRINT REVERSE *************************/
+/**
+ * print_reverse - Prints reverse string.
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
+ */
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
 		count++;
 	}
-	string = malloc((count + 1) * sizeof(char));
-	if (n < 0)
-		string[i++] = '-';
-	while (expo >= 1)
-	{
-		string[i++] = (((res / expo) % 10) * -1 + '0');
-		expo /= 10;
-	}
-	return (string);
+	return (count);
 }
+/************************* PRINT A STRING IN ROT13 *************************/
 /**
-  * found_unsigned - converts unsigned to char arrays
-  * @usign: unsigned int to convert from va_args
-  * Return: pointer to string
-  */
-char *found_unsigned(va_list usign)
+ * print_rot13string - Print a string in rot13.
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of chars printed
+ */
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	unsigned int i, res, temp, expo, count;
-	char *string;
+	char x;
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	res = va_arg(usign, unsigned int);
-	count = i = 0;
-	expo = 1;
-	temp = res;
-	while (temp >= 10)
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		str = "(AHYY)";
+	for (i = 0; str[i]; i++)
 	{
-		expo *= 10;
-		temp /= 10;
-		count++;
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				x = out[j];
+				write(1, &x, 1);
+				count++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			x = str[i];
+			write(1, &x, 1);
+			count++;
+		}
 	}
-	string = malloc((count + 1) * sizeof(char));
-	while (expo >= 1)
-	{
-		string[i++] = (((res / expo) % 10) + '0');
-		expo /= 10;
-	}
-	return (string);
+	return (count);
 }
